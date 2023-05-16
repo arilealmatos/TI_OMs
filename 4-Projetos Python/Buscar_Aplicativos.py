ba90fe7d64186e96605189ec12cbce9574aa3112
Ari Leal Matos <arilealmatos@gmail.com>
2023-05-15 22:13:14 -0300

    19 - Terceiro Envio Python.

---
+++ 4-Projetos Python/Buscar_Aplicativos.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import winapps 
+def app(): 
+  
+    for item in winapps.search_installed(e.get()): 
+        name.set(item.name) 
+        version.set(item.version) 
+        Install_date.set(item.install_date) 
+        publisher.set(item.publisher) 
+        uninstall_string.set(item.uninstall_string) 
+  
+master = Tk() 
+master.title("Buscador de Apps")
+master.configure(bg='light grey') 
+name = StringVar() 
+version = StringVar() 
+Install_date = StringVar() 
+publisher = StringVar() 
+uninstall_string = StringVar() 
+  
+Label(master, text="Digite o Nome do App : ", 
+      bg="light grey").grid(row=0, sticky=W) 
+Label(master, text="Nome : ", 
+      bg="light grey").grid(row=2, sticky=W) 
+Label(master, text="Versão :", 
+      bg="light grey").grid(row=3, sticky=W) 
+Label(master, text="Data de Instalação :", 
+      bg="light grey").grid(row=4, sticky=W) 
+Label(master, text="Fabricante :", 
+      bg="light grey").grid(row=5, sticky=W) 
+Label(master, text="Chave de Desinstalação :", 
+      bg="light grey").grid(row=6, sticky=W) 
+  
+Label(master, text="", textvariable=name, 
+      bg="light grey").grid(row=2, column=1, sticky=W) 
+Label(master, text="", textvariable=version, 
+      bg="light grey").grid(row=3, column=1, sticky=W) 
+Label(master, text="", textvariable=Install_date, 
+      bg="light grey").grid(row=4, column=1, sticky=W) 
+Label(master, text="", textvariable=publisher, 
+      bg="light grey").grid(row=5, column=1, sticky=W) 
+Label(master, text="", textvariable=uninstall_string, 
+      bg="light grey").grid(row=6, column=1, sticky=W) 
+  
+  
+e = Entry(master, width=30) 
+e.grid(row=0, column=1) 
+b = Button(master, text="Buscar", command=app, bg="Blue") 
+b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=5, pady=5,) 
+  
+mainloop() 
