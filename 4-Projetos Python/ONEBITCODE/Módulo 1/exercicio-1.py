451024f08f35287ba8521933eda6bc8d173a37de
Ari Leal Matos <arilealmatos@gmail.com>
2023-12-03 15:05:28 -0300

    46 - Sexto Envio PYTHON.

---
+++ 4-Projetos Python/ONEBITCODE/Módulo 1/exercicio-1.py
@@ -0,0 +1,16 @@
+# Exercício Numero 1
+
+# Antecessor e Sucessor
+numero = int(input("Digite um número\n"))
+
+print(f"Antecessor do número: {numero -1} | Sucessor do número: {numero +1}")
+
+# Media Aritmetica
+nota1 = float(input("Digite a nota 1\n"))
+nota2 = float(input("Digite a nota 2\n"))
+nota3 = float(input("Digite a nota 3\n"))
+nota4 = float(input("Digite a nota 4\n"))
+
+average = (nota1 + nota2 + nota3 + nota4) /4
+
+print(f"Médias das notas é: {average:.2f}")
